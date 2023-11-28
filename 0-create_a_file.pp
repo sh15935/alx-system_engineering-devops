15d41a530fcba358e6ce8652e9124e74c4a191a7
sh15935 <safazsahih@gmail.com>
2023-11-28 11:25:51 -0800

    tasks

--- 0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# creates a file in /tmp
-
-file { '/tmp/school':
-  ensure  => 'present',
-  content =>  'I love Puppet',
-  owner   =>  'www-data',
-  group   =>  'www-data',
-  mode    =>  '0744'
-}
